12a9bb2a89437daaf2c7b275c2e1ae7125298d29
Friedrich Chen <jaleverchen@gmail.com>
2025-03-01 20:28:01 +0800

    feat: mv users to submodules

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -8,11 +8,11 @@ import {
   Delete,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { User } from '../entities/user.entity';
+import { User } from './entities/user.entity';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
 
   @Get()
   findAll(): Promise<User[]> {
