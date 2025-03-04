5bec13d41ed97234a11bb2cfdc1cc1ec6f9365a4
Friedrich Chen <jaleverchen@gmail.com>
2025-03-04 18:04:35 +0800

    feat: remove unused codes

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,37 +1,3 @@
-// import { Injectable } from '@nestjs/common';
-// import { InjectRepository } from '@nestjs/typeorm';
-// import { Repository } from 'typeorm';
-// import { User } from './entities/user.entity';
-
-// @Injectable()
-// export class UsersService {
-//   constructor(
-//     @InjectRepository(User)
-//     private usersRepository: Repository<User>,
-//   ) {}
-
-//   findAll(): Promise<User[]> {
-//     return this.usersRepository.find();
-//   }
-
-//   findOne(id: number): Promise<User> {
-//     return this.usersRepository.findOneBy({ id });
-//   }
-
-//   create(user: User): Promise<User> {
-//     return this.usersRepository.save(user);
-//   }
-
-//   async update(id: number, user: User): Promise<void> {
-//     await this.usersRepository.update(id, user);
-//   }
-
-//   async remove(id: number): Promise<void> {
-//     await this.usersRepository.delete(id);
-//   }
-// }
-
-// users/users.service.ts
 import { Injectable, ConflictException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
